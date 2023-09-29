14c6a55f25193cbbd5081bb8a894605208453537
lluis82 <ltermar@etsinf.upv.es>
2023-09-29 09:25:07 +0200

    initial commit

---
+++ src/main/java/com/greenit/greenitapi/GreenItApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenit.greenitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenItApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenItApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenit/greenitapi/GreenItApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenit.greenitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenItApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
