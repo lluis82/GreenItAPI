518d540bb16dde5bd362d0519e702a5977e225c1
Windows 7 Enjoyer <pabloroigbondia@hotmail.com>
2023-10-12 12:14:31 +0200

    limpieza de mi codigo sucio

--- src/main/java/com/greenit/greenitapi/Services/UserService.java
+++ src/main/java/com/greenit/greenitapi/Services/UserService.java
@@ -2,10 +2,10 @@ package com.greenit.greenitapi.Services;
 
 import com.greenit.greenitapi.Entities.User;
 import com.greenit.greenitapi.Util.Config;
+import com.greenit.greenitapi.Util.mariadbConnect;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Optional;
@@ -13,53 +13,43 @@ import java.util.Optional;
 @Service
 public class UserService {
     private static Connection connection;
-
     private static Config config = new Config();
 
-    public UserService(){
+    public UserService() {
     }
 
     public Optional<User> getUser(String emailIn) {
-
-        //esto es una mierda y lo tengo que mover de sitio peeeero no me importa rn
-        //also usar properties / config
-        Optional<User> optional = Optional.empty();
+        Optional<User> optional;
         User user = null;
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost:3306/merequetengue",
-                    "root", "adre1234"
-            );
-        }catch(Exception e){
-            System.out.println("Error al conectar con la BD de mariaDB");
-        }
-
-
-
+        connection = mariadbConnect.mdbconn();
 
         try (PreparedStatement statement = connection.prepareStatement("""
-            SELECT *
-            FROM Users u
-            WHERE u.email like ?
-        """)) {
-            statement.setString(1,emailIn);//esto es para reemplazar el ? por el email
+                    SELECT *
+                    FROM Users u
+                    WHERE u.email like ?
+                """)) {
+
+            statement.setString(1, emailIn);
+            //esto es para reemplazar el ? por el email
             //es muy cursed i know
+
             ResultSet resultSet = statement.executeQuery();
+
             while (resultSet.next()) {
                 int id = resultSet.getInt(1); // by column index
                 String name = resultSet.getString("userName"); // by column name
                 String email = resultSet.getString("email");
                 String password = resultSet.getString("password");
-
+/*
                 System.out.println("--== DATOS ==--");
                 System.out.println("ID:" + id);
                 System.out.println("Nombre:" + name);
                 System.out.println("Email:" + email);
                 System.out.println("Password:" + password);
-
+*/
                 user = new User(name, email, password, config.getSrvName());
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error al recuperar info de la BD");
         }
         optional = Optional.of(user);
