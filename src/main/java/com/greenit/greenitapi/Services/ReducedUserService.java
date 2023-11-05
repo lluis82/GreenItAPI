6426e5d80fe8aede7077f8e14ed3ae4bd988dcbb
lluis82 <ltermar@etsinf.upv.es>
2023-11-05 12:28:18 +0100

    hecho services reducedUser :)

---
+++ src/main/java/com/greenit/greenitapi/Services/ReducedUserService.java
@@ -0,0 +1,123 @@
+package com.greenit.greenitapi.Services;
+
+import com.greenit.greenitapi.Entities.ReducedUser;
+import com.greenit.greenitapi.Entities.Step;
+import com.greenit.greenitapi.Entities.User;
+import com.greenit.greenitapi.Util.Base64machine;
+import com.greenit.greenitapi.Util.Config;
+import com.greenit.greenitapi.Util.mariadbConnect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ReducedUserService {
+
+    private static Connection connection;
+    private static Config config = new Config();
+
+
+    // id -> List<ReducedUser> pibe followea a los pibes de la lista (mirar codigo server/steps de pablo)
+    public static Optional<List<ReducedUser>> getFollowedbyUser(int userId) {
+        Optional<List<ReducedUser>> optional;
+        List<ReducedUser> sol = new ArrayList<>();
+        connection = mariadbConnect.mdbconn();
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    SELECT *
+                    FROM Follows f
+                    WHERE f.User = ?
+                """)) {
+            statement.setInt(1,userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next() == false) {
+                return null;
+            } else {
+                do {
+                    int follows = resultSet.getInt("Follows");
+                    User u = UserService.getUserById(follows).orElse(null);
+                    ReducedUser ru = new ReducedUser(u.getDisplayName(), u.getImage(), u.getImagefield());
+                    sol.add(ru);
+                } while (resultSet.next());
+                connection.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al recuperar info de la BD");
+        }
+        optional = Optional.of(sol);
+        return optional;
+    }
+
+    // id -> List<ReducedUser> lista de pibes que followea a id
+    public static Optional<List<ReducedUser>> getUserFollowes(int userId) {
+        Optional<List<ReducedUser>> optional;
+        List<ReducedUser> sol = new ArrayList<>();
+        connection = mariadbConnect.mdbconn();
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    SELECT *
+                    FROM Follows f
+                    WHERE f.Follows = ?
+                """)) {
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next() == false) {
+                return null;
+            } else {
+                do {
+                    int user = resultSet.getInt("User");
+                    User u = UserService.getUserById(user).orElse(null);
+                    ReducedUser ru = new ReducedUser(u.getDisplayName(), u.getImage(), u.getImagefield());
+                    sol.add(ru);
+                } while (resultSet.next());
+                connection.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al recuperar info de la BD");
+        }
+        optional = Optional.of(sol);
+        return optional;
+    }
+
+    // id -> empieza a followear a otra id
+    public String newFollower(int userId, int followedUserId) {
+        connection = mariadbConnect.mdbconn();
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    INSERT INTO Follows (User, Follows) VALUES (?, ?)
+                """)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, followedUserId);
+            ResultSet resultSet = statement.executeQuery();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error al recuperar info de la BD");
+            return config.getSrvName() + " FAIL, Excepción: " + e.getMessage();
+        }
+        return config.getSrvName() + " OK";
+
+    }
+
+    //id -> deja de followear a otra id
+    public String deleteFollower(int userId, int unfollowedUserId) {
+        connection = mariadbConnect.mdbconn();
+        try (PreparedStatement statement = connection.prepareStatement("""
+                    DELETE * FROM Follows WHERE User = ? AND Follows = ?
+                """)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, unfollowedUserId);
+            ResultSet resultSet = statement.executeQuery();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error al borrar info de la BD");
+            return config.getSrvName() + " FAIL, Excepción: " + e.getMessage();
+        }
+        return config.getSrvName() + " OK";
+
+    }
+
+}
