7922e4fb1288c597e723f828487249e8cf6b0279
odio git, ipc, dsm y mes <proicer@etsinf.upv.es>
2023-11-29 12:53:42 +0100

    -auto restore aws database para pruebas

--- src/test/java/com/greenit/greenitapi/GreenItApiApplicationTests.java
+++ src/test/java/com/greenit/greenitapi/GreenItApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.greenit.greenitapi;
 import com.greenit.greenitapi.Controller.*;
 import com.greenit.greenitapi.Entities.User;
 import com.greenit.greenitapi.Util.Config;
+import com.greenit.greenitapi.Util.mariadbConnect;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -15,6 +16,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.Duration;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -50,6 +57,9 @@ class GreenItApiApplicationTests {
     @Test
     public void testBackendCache() throws Exception{
         mockMvc.perform(get("http://localhost:8080/purgecache")).andExpect(status().isOk()).andExpect(content().string(new Config().getSrvName() + " OK"));
+
+        resetDatabase();
+
         //region server
         ServerController.meetServer("192.168.1.1","Eruruu");
         var a = ServerController.getServers();
@@ -272,6 +282,47 @@ class GreenItApiApplicationTests {
         Thread.sleep(2000);
 
         webdriver.close();
+    }
+
+    public static void resetDatabase() throws SQLException {
+        String s;
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            FileReader fr = new FileReader(new File(Config.getResourcesLocation() + "awsFinal.sql"));
+            // be sure to not have line starting with "--" or "/*" or any other non aplhabetical character
+
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((s = br.readLine()) != null) {
+                sb.append(s);
+            }
+            br.close();
+
+            // here is our splitter ! We use ";" as a delimiter for each request
+            // then we are sure to have well formed statements
+            String[] inst = sb.toString().split(";");
+
+            Connection c = mariadbConnect.mdbconn();
+            Statement st = c.createStatement();
+
+            for (int i = 0; i < inst.length; i++) {
+                // we ensure that there is no spaces before or after the request string
+                // in order to not execute empty statements
+                if (!inst[i].trim().equals("")) {
+                    st.executeUpdate(inst[i] + ";");
+                    System.out.println(">>" + inst[i] + ";");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("*** Error : " + e);
+            System.out.println("*** ");
+            System.out.println("*** Error : ");
+            e.printStackTrace();
+            System.out.println("################################################");
+            System.out.println(sb);
+        }
     }
 
 }
